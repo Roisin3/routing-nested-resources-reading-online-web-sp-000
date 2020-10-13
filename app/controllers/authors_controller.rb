29f58b5f81c284add52b7290103dc53d645414ce
Michael Lynch <michael.lynch8211@gmail.com>
2020-10-13 20:57:08 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -4,16 +4,4 @@ class AuthorsController < ApplicationController
     @author = Author.find(params[:id])
   end
 
-  def posts_index
-    @author = Authoer.find(params[:id])
-    @posts = @author.posts
-    render template: 'posts/index'
-  end
-
-  def post
-    @author = Author.find(params[:id])
-    @post = Post.find(params[:post_id])
-    render template: 'posr/show'
-  end
-
 end
